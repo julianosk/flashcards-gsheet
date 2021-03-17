b79a6375f3e0d36975e8c2582599974bc7eec72e
Juliano Krieger <julianosk@gmail.com>
2021-03-17 22:29:58 +0100

    Added style to Flashcards.tsx

--- ui/src/components/main/home/Flashcards.tsx
+++ ui/src/components/main/home/Flashcards.tsx
@@ -1,11 +1,41 @@
-import { Container } from "@material-ui/core";
+import { Container, CssBaseline, Divider, Paper, makeStyles } from "@material-ui/core";
 import React from "react";
 
+const useStyles = makeStyles((theme) => ({
+  main: {
+    marginBottom: theme.spacing(4),
+  },
+  paper: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(2),
+    [theme.breakpoints.up('md')]: {
+      marginTop: theme.spacing(6),
+      marginBottom: theme.spacing(6),
+      padding: theme.spacing(3),
+    },
+  },
+}));
 
 export function Flashcards() {
+  const classes = useStyles();
   return (
-    <Container>
-      Flashcards
-    </Container>
+    <React.Fragment>
+      <CssBaseline/>
+      <Container
+        component="main"
+        className={classes.main}
+        maxWidth="sm"
+      >
+        <Paper
+          className={classes.paper}
+          variant="outlined"
+        >
+          Dutch
+          <Divider/>
+          Translation
+        </Paper>
+      </Container>
+    </React.Fragment>
   );
 }
